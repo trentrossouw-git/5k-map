c92f540ee8bf149d8801dec56b050693b22c4a39
trentrossouw-git <trent.rossouw@gmail.com>
2026-08-23 18:23:22 +1000

    Add 'temp-name' entry to TEAMS configuration

--- js/teams-config.js
+++ js/teams-config.js
@@ -18,7 +18,8 @@ const SHEET_ID = "1Hrrww0MC2LItlEdhl5xCcw12qad4A1aitvbKodyO5Iw";
 const TEAMS = {
   "close-enough": { label: "Close Enough", tab: "Close Enough" },
   "catfishermen": { label: "Catfishermen", tab: "Catfishermen" },
-  "homework-help": { label: "Homework Help", tab: "Homework Help" }
+  "homework-help": { label: "Homework Help", tab: "Homework Help" },
+  "temp-name": { label: "Allan and Lila", tab: "TempName" }
 };
 
 function csvUrlForTab(tabName) {
